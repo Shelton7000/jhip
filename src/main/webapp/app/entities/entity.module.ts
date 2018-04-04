7990c29c39acf89ba15352e6f404bd233cc7d578
HP2000\shelton <hoagie7000@gmail.com>
2018-04-04 02:29:43 -0400

    entity

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,25 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { BlogRegionModule } from './region/region.module';
+import { BlogCountryModule } from './country/country.module';
+import { BlogLocationModule } from './location/location.module';
+import { BlogDepartmentModule } from './department/department.module';
+import { BlogTaskModule } from './task/task.module';
+import { BlogEmployeeModule } from './employee/employee.module';
+import { BlogJobModule } from './job/job.module';
+import { BlogJobHistoryModule } from './job-history/job-history.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        BlogRegionModule,
+        BlogCountryModule,
+        BlogLocationModule,
+        BlogDepartmentModule,
+        BlogTaskModule,
+        BlogEmployeeModule,
+        BlogJobModule,
+        BlogJobHistoryModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
